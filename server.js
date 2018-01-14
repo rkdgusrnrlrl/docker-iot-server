de8a9ab34774f0fc7b0766556926abdd26bd1815
강현구 <rkdgusrnrlrl@gmail.com>
2018-01-14 20:21:16 +0900

    fix server source for test

--- server.js
+++ server.js
@@ -3,4 +3,12 @@ const app = express()
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
-app.listen(80, () => console.log('Example app Klistening on port 80!'))
+
+let port = process.env.PORT || (process.argv[2] || 3000);
+port = (typeof port === "number") ? port : 3000;
+
+if (!module.parent) { app.listen(port) }
+console.log(`Example app Klistening on port ${port}!`)
+
+
+module.exports = app;
