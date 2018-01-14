3c38dd9d6e89727a3dfbb4f020d0d56b53387e24
강현구 <rkdgusrnrlrl@gmail.com>
2018-01-14 20:23:42 +0900

    remove semicolon

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const app = express()
 app.get('/', (req, res) => res.send('Hello World!'))
 
 
-let port = process.env.PORT || (process.argv[2] || 3000);
-port = (typeof port === "number") ? port : 3000;
+let port = process.env.PORT || (process.argv[2] || 3000)
+port = (typeof port === "number") ? port : 3000
 
 if (!module.parent) { app.listen(port) }
 console.log(`Example app Klistening on port ${port}!`)
 
 
-module.exports = app;
+module.exports = app
