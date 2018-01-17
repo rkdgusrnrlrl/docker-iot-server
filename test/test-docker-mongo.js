dd9476d5c0df52332faa250bc51a06c8d8d5edd3
강현구 <rkdgusrnrlrl@gmail.com>
2018-01-18 00:29:36 +0900

    add mongoose test

---
+++ test/test-docker-mongo.js
@@ -0,0 +1,46 @@
+/**
+ * Created by rkdgusrnrlrl on 18. 1. 14.
+ */
+const chai = require('chai');
+const expect = chai.expect;
+const Docker = require('dockerode');
+
+let container;
+
+describe('mongo docker test', async function() {
+    let mongoContainer;
+    before("", async function() {
+        const docker = new Docker({
+            socketPath: '/var/run/docker.sock'
+        });
+
+        mongoContainer = await docker.createContainer({
+            Image : "mongo",
+            ExposedPorts: {
+                "27017/tcp": {}
+            },
+            HostConfig : {
+                "PortBindings": { "27017/tcp": [{ "HostPort": "27017" }] },
+            }
+        })
+        await mongoContainer.start()
+    })
+
+    it('save document', async function() {
+
+        const mongoose = require('mongoose');
+        mongoose.connect('mongodb://localhost/test', { useMongoClient: true });
+        mongoose.Promise = global.Promise;
+
+        const Cat = mongoose.model('Cat', { name: String })
+        const kitty = new Cat({ name: 'Zildjian' })
+        await kitty.save()
+
+        mongoose.disconnect()
+
+    })
+
+    after("remove mongoContainer", async function () {
+        await mongoContainer.remove({force :true})
+    })
+})
