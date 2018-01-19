973df3074bdefc0f297efdab0922a8ca3c8ef09a
강현구 <rkdgusrnrlrl@gmail.com>
2018-01-19 21:32:36 +0900

    test is pass t-hat POST /air-status is ok

--- server.js
+++ server.js
@@ -3,12 +3,18 @@ const app = express()
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
+app.post('/air-status', (req, res) => {
+    res.json({ok : true})
+})
+
 
 let port = process.env.PORT || (process.argv[2] || 3000)
 port = (typeof port === "number") ? port : 3000
 
 if (!module.parent) { app.listen(port) }
 console.log(`Example app Klistening on port ${port}!`)
+
+
 
 
 module.exports = app
