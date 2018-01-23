216090230c843f4e3d0900c64e5841d8823153d8
HyunKu Kang <rkdgusrnrlrl@gmail.com>
2018-01-24 00:04:16 +0900

    add require error handler

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
 app.post('/air-status', (req, res) => {
+
+    const temp = parseInt(req.body.temp, 10)
+    const hum = parseInt(req.body.hum, 10)
+
+    let errors = [];
+    if (isNaN(temp)) errors.push({temp : "REQUIRE"})
+    if (isNaN(hum)) errors.push({hum : "REQUIRE"})
+    if(errors.length > 0) return res.status(400).json({ errors : errors })
+
+
     res.json({ok : true})
 })
 
