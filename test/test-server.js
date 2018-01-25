a4d3253f96238efcb4ec0a2c435375459ae87f25
HyunKu Kang <rkdgusrnrlrl@gmail.com>
2018-01-25 22:22:06 +0900

    add test mongo add sencer datat

--- test/test-server.js
+++ test/test-server.js
@@ -49,4 +49,42 @@ describe('server start test', function() {
             })
             .catch(done)
     })
+
+    it('POST /air-status save data', async function() {
+        const Docker = require('dockerode');
+        const docker = new Docker({
+            socketPath: '/var/run/docker.sock'
+        });
+
+        const container = await docker.createContainer({
+            Image : "mongo",
+            ExposedPorts: {
+                "27017/tcp": {}
+            },
+            HostConfig : {
+                "PortBindings": { "27017/tcp": [{ "HostPort": "27017" }] },
+            }
+        })
+        await container.start()
+
+        await request(app)
+            .post('/air-status')
+            .set('Accept', 'application/json')
+            .send({temp : 17, hum : 55})
+            .expect(200)
+
+        const mongoose = require('mongoose');
+        mongoose.Promise = global.Promise;
+        mongoose.connect('mongodb://localhost/iot', { useMongoClient: true });
+
+        const Sencer = mongoose.model('sencer', {  temp : Number ,hum: Number })
+        const sencerDataList = await Sencer.find({}).lean().exec();
+
+        expect(sencerDataList).to.have.lengthOf(1)
+
+        mongoose.disconnect()
+
+        await container.remove({force : true})
+    })
+
 })
